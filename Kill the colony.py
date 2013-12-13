3d57829acdb72f1c03ba0bbb82afc1de6109512d
HissingCockroach <11roadkills@gmail.com>
2013-12-13 11:17:35 -0500

    Update Kill the colony.py

--- Kill the colony.py
+++ Kill the colony.py
@@ -4,7 +4,6 @@ import sys
 import time
 import pygame
 
-
 def image_from_url(url):
     try:
         from urllib2 import urlopen
@@ -16,11 +15,13 @@ def image_from_url(url):
     return inout(myurl.read())
 
 
+
+
+
 Bug_URL = ('http://i1315.photobucket.com/albums/t600/11roadkills/9705abeb-8eab-4379-afe2-b8355bcd6b4f_zps86fb3a66.jpg?t=1382655213')
 Flyswatter_URL = ('http://i1315.photobucket.com/albums/t600/11roadkills/Flyswatter_zps8c0c501f.png')
 Hit_URL = ('http://i1315.photobucket.com/albums/t600/11roadkills/683a3bd2-c45d-4770-b5f1-66bdef066628_zpsf94850e3.jpg?t=1384560988')
-
-
+Splat_URL = ('http://i1315.photobucket.com/albums/t600/11roadkills/Splat_zpsbdf5c74a.png')
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -33,9 +34,11 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((900,700),0,32)
 pygame.display.set_caption('Kill The Colony')
 clock = pygame.time.Clock()
-
+u = True
+d = False
 level = 2
 score = 0
+splat_image = pygame.image.load(image_from_url(Splat_URL))
 bug_image = pygame.image.load(image_from_url(Bug_URL))
 Flyswatter = pygame.image.load(image_from_url(Flyswatter_URL))
 Flyswatterhit = pygame.image.load(image_from_url(Hit_URL))
@@ -45,7 +48,8 @@ myfont3 = pygame.font.SysFont("Comic Sans MS", 80)
 place = [(800,298) , (800,200) , (800,80) , (800,500)]
 setplace = random.choice(place)
 Flyswatter_strike = pygame.transform.rotate(Flyswatter, 180)
-label = myfont.render("Press P to play", 1 , BLACK)
+label = myfont.render("Play", 1 , BLACK)
+label4 = myfont.render("Quit", 1, BLACK)
 label3 = myfont3.render("Kill The Colony", 1, YELLOW)
 label2 = myfont2.render("GAME OVER", 1, RED)
 wat = 0
@@ -54,17 +58,11 @@ bug_rect = bug_image.get_rect(topleft=(setplace))
 hit = False
 l = False
 strike = False
-r = open("Output.txt", "r")
-readit = r.read()
 p = False
 while True:
     DISPLAYSURF.fill(WHITE)
     key = pygame.key.get_pressed()
 
-        
-    if not bug_rect.x <= -25 and key[pygame.K_p]:
-        p = True
-        l = True
     if l and key[pygame.K_UP]:
         flyswatter_rect.y -= level
     if l and key[pygame.K_DOWN]:
@@ -84,11 +82,26 @@ while True:
     if p == False:
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(label3, (150,100))
-        DISPLAYSURF.blit(label, (250,550))
+        DISPLAYSURF.blit(label, (360,550))
+        DISPLAYSURF.blit(label4, (360,580))
         Flyswatter_hit = pygame.transform.rotate(Flyswatterhit, 290)
         DISPLAYSURF.blit(Flyswatter_hit, (250,200))
-
-    
+        if key[pygame.K_UP]:
+            u = True
+            d = False
+        if u:
+            pygame.draw.rect(DISPLAYSURF, BLACK, (350, 550, 80, 30,), 10)
+            if key[pygame.K_RETURN]:
+                p = True
+                l = True
+                
+        if key[pygame.K_DOWN]:
+            d = True
+            u = False
+        if d:
+            pygame.draw.rect(DISPLAYSURF, BLACK, (350, 580, 80, 30,), 10)
+            if key[pygame.K_RETURN]:
+                pygame.quit()
     
     if key[pygame.K_SPACE] and flyswatter_rect.colliderect(bug_rect):
         place = [(800,500) , (800,80) , (800,200) , (800,298)]
@@ -99,40 +112,44 @@ while True:
         Flyswatter_hit = pygame.transform.rotate(Flyswatterhit, 290)
         DISPLAYSURF.blit(Flyswatter_hit, flyswatter_rect)
         DISPLAYSURF.blit(bug_image, bug_rect)
-        
-    r = open("Output.txt", "r")
-    readit = r.read()
-    if score >= r.read():
-        r = open("Output.txt", "r")
-        readit = r.read()
-        w = open("Output.txt", "w")
-        w.write("{}".format(score))
 
         
+
     if p:    
         scoreboard = myfont.render('Score: {}'.format(score), 1 , BLACK)
         DISPLAYSURF.blit(scoreboard, (10,10))
-        if score >= r.read():
-            r = open("Output.txt", "r")
-            readit = r.read()
-            w = open("Output.txt", "w")
-            w.write("{}".format(score))
         
-    if key[pygame.K_h]:
-        read = open("Output.txt", "r")
-        readit = read.read()
-        highscore = myfont.render('Highscore:{}'.format(readit), 1 , BLACK)
-        DISPLAYSURF.blit(highscore, (700, 10))
-        if score >= read.read():
-            read = open("Output.txt", "r")
-            readit = read.read()
-            w = open("Output.txt", "w")
-            w.write("{}".format(score))
-  
     if bug_rect.x <= -25:
             l = False
-            DISPLAYSURF.blit(label2, (200,350))
-
+            t = True
+            a = True
+            if t and a:
+                label = myfont.render("Play Again", 1 , BLACK)
+                DISPLAYSURF.blit(label2, (200,350))
+                DISPLAYSURF.blit(label, (370,550))
+                DISPLAYSURF.blit(label4, (430,580))
+            if key[pygame.K_UP]:
+                u = True
+                d = False
+                
+            if u:
+                pygame.draw.rect(DISPLAYSURF, BLACK, (360, 550, 220, 30,), 10)
+                if key[pygame.K_RETURN]:
+                    bug_rect = bug_image.get_rect(topleft=(setplace))
+                    flyswatter_rect = Flyswatter.get_rect(topleft=(-25,298))
+                    level = 2
+                    score = 0
+                    p = True
+                    l = True
+                    t = False
+                    a = False
+            if key[pygame.K_DOWN]:
+                d = True
+                u = False
+            if d:
+                pygame.draw.rect(DISPLAYSURF, BLACK, (430, 580, 80, 30,), 10)
+                if key[pygame.K_RETURN]:
+                    pygame.quit()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
